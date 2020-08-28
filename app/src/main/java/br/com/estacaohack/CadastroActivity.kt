d3336e31ef6d8dedeaa29c3773dc3ff90159e0ba
Fernando Batista <nandosbx@gmail.com>
2020-08-27 23:01:05 -0300

    Login Actions

--- app/src/main/java/br/com/estacaohack/CadastroActivity.kt
+++ app/src/main/java/br/com/estacaohack/CadastroActivity.kt
@@ -2,6 +2,7 @@ package br.com.estacaohack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/br/com/estacaohack/LoginActivity.kt
+++ app/src/main/java/br/com/estacaohack/LoginActivity.kt
@@ -2,10 +2,32 @@ package br.com.estacaohack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //Executing the clicking button
+        btnLoginEntrar.setOnClickListener {
+
+            //Input data
+            val email = edtLoginEmail.text.toString().trim().toLowerCase()
+            val senha = edtLoginSenha.text.toString().trim()
+
+            //Validations
+            if(email.isEmpty()){
+                edtLoginEmail.error = "Campo obrigatório."
+                edtLoginEmail.requestFocus()
+            } else if (senha.isEmpty()){
+                edtLoginSenha.error = "Campo obrigatório."
+                edtLoginSenha.requestFocus()
+            } else {
+                //Showing error message to user
+                Toast.makeText(this, "E-mail ou senha inválidos.", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
