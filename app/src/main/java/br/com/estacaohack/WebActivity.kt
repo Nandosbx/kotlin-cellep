8a3a44eb05582a14dee4f3489ffb00d4c7126aa0
Fernando Batista <nandosbx@gmail.com>
2020-09-01 20:49:49 -0300

    Web Activity Button Link

--- app/src/main/java/br/com/estacaohack/WebActivity.kt
+++ app/src/main/java/br/com/estacaohack/WebActivity.kt
@@ -2,10 +2,21 @@ package br.com.estacaohack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_web.*
 
 class WebActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
+
+        //Habilitando a execução de códigos Javascript
+        wbvWeb.settings.javaScriptEnabled = true
+
+        //Carregando um endereço Web
+        wbvWeb.loadUrl("http://br.cellep.com/estacaohack")
+
+        //Definindo o WebView como cliente web padrão
+        wbvWeb.webViewClient = WebViewClient()
     }
 }
