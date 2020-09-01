87e7a50faf02e718f50ce0b8317fe5c8cd111bb7
Fernando Batista <nandosbx@gmail.com>
2020-09-01 19:34:58 -0300

    Web Activity

--- app/src/main/java/br/com/estacaohack/MainActivity.kt
+++ app/src/main/java/br/com/estacaohack/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.estacaohack
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Recuperar o email passado por meio da Intent
+        val email = intent.getStringArrayExtra("INTENT_EMAIL")
+
+        //Acessar o arquivo de preferências compartilhadas
+        val sharedPrefs = getSharedPreferences(
+            "cadastro_$email", //Nome do arquivo
+            Context.MODE_PRIVATE //Modo de acesso
+        )
+
+        //Recuperar dados no arquivo de preferências compartilhadas
     }
 }
